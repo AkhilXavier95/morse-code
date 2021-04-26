376ba032dcf8c2ef226f38ca09bee9b2d4333869
AkhilXavier95 <akhilxavier95@gmail.com>
2021-04-27 03:27:45 +0530

    app bar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,27 @@
-import logo from "assets/logo.svg";
-import "css/App.css";
+import React from "react";
+
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+
+import useStyles from "./style";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="fixed" color="primary" className={classes.appBar}>
+        <Toolbar>
+          <Fab color="secondary" aria-label="add" className={classes.fabButton}>
+            <AddIcon />
+          </Fab>
+          <div className={classes.grow} />
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
   );
 }
 
