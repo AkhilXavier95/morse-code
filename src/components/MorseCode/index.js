8415cfd834d9dfa1787a10e7bdbb29f6fe23b45f
AkhilXavier95 <akhilxavier95@gmail.com>
2021-04-28 23:54:46 +0530

    basic working

--- src/components/MorseCode/index.js
+++ src/components/MorseCode/index.js
@@ -1,33 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
+import { Typography } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+
 import RemoveIcon from "@material-ui/icons/Remove";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import TextField from "@material-ui/core/TextField";
-import { Typography } from "@material-ui/core";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
 
 import { useStyles } from "./styles";
+import { learArray, morseJson } from "utils/helpers";
 
 const MorseCode = () => {
   const classes = useStyles();
+
+  const [item, setItem] = useState("a");
+  const [value, setValue] = useState("");
+
+  const handleButtonClick = (symbol) => {
+    console.log({ symbol });
+    if (symbol === 0) {
+      setValue(value + "-");
+    } else {
+      setValue(value + ".");
+    }
+  };
+
+  const handleClear = () => {
+    const str = value.substring(0, value.length - 1);
+    setValue(str);
+  };
+
+  const handlePrev = () => {
+    const index = learArray.indexOf(item);
+    setItem(learArray[index - 1]);
+    setValue("");
+  };
+
+  const handleNext = () => {
+    const index = learArray.indexOf(item);
+    setItem(learArray[index + 1]);
+    setValue("");
+  };
+
+  const handleSubmit = () => {
+    if (morseJson[item] && morseJson[item] === value) {
+      handleNext();
+    }
+  };
+
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
+    <div className={classes.container}>
       <Paper className={classes.paper} elevation={3}>
-        <Typography className={classes.alphabets}>A</Typography>
+        <Typography className={classes.alphabets}>
+          {item.toUpperCase()}
+        </Typography>
         <div className={classes.inputContainer}>
           <TextField
             error={false}
             id="outlined-error"
-            defaultValue="Hello World"
             variant="outlined"
+            value={value}
             inputProps={{
               style: {
                 height: 0,
@@ -38,23 +73,54 @@ const MorseCode = () => {
             className={classes.button}
             variant="contained"
             color="primary"
+            onClick={handleSubmit}
           >
             OK
           </Button>
         </div>
       </Paper>
       <div className={classes.buttonContainer}>
-        <Button className={classes.button} variant="contained" color="primary">
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          onClick={() => handleButtonClick(0)}
+        >
           <RemoveIcon />
         </Button>
-        <Button className={classes.button} variant="contained" color="primary">
-          <FiberManualRecordIcon />
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          onClick={() => handleButtonClick(1)}
+        >
+          <FiberManualRecordIcon style={{ fontSize: 10 }} />
         </Button>
       </div>
       <div style={{ display: "flex", justifyContent: "center", marginTop: 15 }}>
-        <Button className={classes.buttonClear} variant="contained">
+        <Button
+          className={classes.buttonClear}
+          variant="contained"
+          onClick={handleClear}
+        >
           clear
         </Button>
+      </div>
+      <div className={classes.navigationButtonContainer}>
+        <IconButton
+          aria-label="prev"
+          className={classes.nvaButton}
+          onClick={handlePrev}
+        >
+          <NavigateBeforeIcon />
+        </IconButton>
+        <IconButton
+          aria-label="next"
+          className={classes.nvaButton}
+          onClick={handleNext}
+        >
+          <NavigateNextIcon />
+        </IconButton>
       </div>
     </div>
   );
--- src/components/MorseCode/styles.js
+++ src/components/MorseCode/styles.js
@@ -1,6 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
+  container: {
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
   paper: {
     width: "80%",
     margin: "10px auto",
@@ -31,5 +38,16 @@ export const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     paddingTop: 5,
     paddingBottom: 5,
+  },
+  navigationButtonContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "80%",
+    marginTop: 50,
+  },
+  nvaButton: {
+    background: "#0000000a",
+    boxShadow:
+      "0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)",
   },
 }));
